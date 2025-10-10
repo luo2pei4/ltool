079e9a61dcb9cc64d19f8f984256b79fb1d6b9f7
edemund-luo <edmund@ubix.cn>
2025-10-10 16:38:09 +0800

    change code arch

---
+++ view/navi.go
@@ -0,0 +1,34 @@
+package view
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
+
+type Navi struct {
+	Title   string
+	Content func() View
+}
+
+type firstScreen struct{}
+
+func createFirstScreen() View {
+	return &firstScreen{}
+}
+
+var (
+	NaviItems = map[string]Navi{
+		"lustre": {"Lustre", createFirstScreen},
+		"node":   {"Node", NewNodesUI},
+	}
+	NaviItemsIndex = map[string][]string{
+		"":       {"lustre"},
+		"lustre": {"node"},
+	}
+)
+
+func (f *firstScreen) CreateView(w fyne.Window) fyne.CanvasObject {
+	return widget.NewLabel("")
+}
+
+func (f *firstScreen) Cleanup() {}
