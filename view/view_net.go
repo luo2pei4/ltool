19bd6cb5c27d64d53355ad35b57ea4699b723bbc
luopei <luo2pei4321@outlook.com>
2025-10-23 22:24:01 +0800

    temporary commit

--- view/view_net.go
+++ view/view_net.go
@@ -3,9 +3,12 @@ package view
 import (
 	"encoding/json"
 	"fmt"
+	"image/color"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	logger "github.com/luo2pei4/ltool/pkg/log"
@@ -33,25 +36,40 @@ func (v *NetMainUI) CreateView(w fyne.Window) fyne.CanvasObject {
 		logger.Errorf("load node list failed, %v\n", err)
 	}
 	v.searchBtn = widget.NewButtonWithIcon("", theme.SearchIcon(), func() {
-		netInfo, ok := v.state.NodeNet[v.nodeList.Text]
-		if !ok {
-			netInfo = state.NetInfo{}
-		}
-		if err := netInfo.LoadLnetCtlInfo(); err != nil {
-			logger.Errorf("load lnetctl info failed, %v\n", err)
-			return
-		}
-		if err := netInfo.LoadLinkInfo(); err != nil {
-			logger.Errorf("load link info failed, %v\n", err)
-			return
-		}
-		v.state.NodeNet[v.nodeList.Text] = netInfo
-		data, err := json.MarshalIndent(&netInfo, "", "  ")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(string(data))
+		popup := showProgressing(w, "Searching, please wait...", 400)
+		go func() {
+			netInfo, ok := v.state.NodeNet[v.nodeList.Text]
+			if !ok {
+				netInfo = state.NetInfo{}
+			}
+			err := netInfo.LoadLnetCtlInfo()
+			if err != nil {
+				logger.Errorf("load lnetctl info failed, %v\n", err)
+			} else {
+				err = netInfo.LoadLinkInfo()
+				if err != nil {
+					logger.Errorf("load link info failed, %v\n", err)
+				}
+			}
+			v.state.NodeNet[v.nodeList.Text] = netInfo
+			if data, err := json.MarshalIndent(&netInfo, "", "  "); err == nil {
+				fmt.Println(string(data))
+			}
+			fyne.Do(func() {
+				if popup != nil {
+					popup.Hide()
+				}
+				if ok && err != nil {
+					// draw error dialog
+					errLabel := widget.NewLabel(err.Error())
+					errLabel.Wrapping = fyne.TextWrapWord
+					bg := canvas.NewRectangle(color.NRGBA{0, 0, 0, 0})
+					bg.SetMinSize(fyne.NewSize(400, 160))
+					content := container.NewStack(bg, container.NewVBox(errLabel))
+					dialog.ShowCustom("Error", "Close", content, w)
+				}
+			})
+		}()
 	})
 	inputArea := container.NewGridWithColumns(2, v.nodeList, v.searchBtn)
 	content := container.NewBorder(
